275c6527f0b30735ae6d1aa7ddd025a4e9741783
ralph <sanghwaj222@gmail.com>
2023-11-15 19:06:40 +0900

    231115 통계학Silver3

--- 2108통계학.py
+++ 2108통계학.py
@@ -6,24 +6,31 @@ for i in range(N):
     nums.append(int(input()))
 
 #평균값 탐색
-Ever_num = sum(nums) // len(nums)
-answer.append(Ever_num)
+Ever_num = sum(nums) / len(nums)
+answer.append(round(Ever_num))
 
 #중앙값 탐색
-nums.sort
-Middle_num = nums[len(nums)//2]
-answer.append(Middle_num)
+if len(nums) >= 2:
+    nums.sort()
+    Middle_num = nums[len(nums)//2]
+    answer.append(Middle_num)
+else:
+    answer.append(nums[0])
 
 #최빈값 탐색
-if Counter(nums).most_common(1)[1] == Counter(nums).most_common(2)[1]:
-    Alot_num = Counter(nums).most_common(2)[0]
+if len(nums) >= 2:
+    if Counter(nums).most_common(1)[0][1] == Counter(nums).most_common(2)[1][1]:
+        Alot_num = Counter(nums).most_common(2)[1][0]
+    else:
+        Alot_num = Counter(nums).most_common(1)[0][0]
+    answer.append(Alot_num)
 else:
-    Alot_num = Counter(nums).most_common(1)[0]
-answer.append(Alot_num)
+    answer.append(nums[0])
 
 #범위 탐색 (최댓값,최솟값)
 Range_num = max(nums) - min(nums)
 answer.append(Range_num)
 
 for i in range(4):
-    print(answer[i])    
+    print(answer[i])
+print(round(2.7))

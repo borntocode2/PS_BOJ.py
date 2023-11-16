e129d9816161cbcc186589cbe5be958e3b843a77
ralph <sanghwaj222@gmail.com>
2023-11-16 21:21:18 +0900

    231116 greedy Silver1

---
+++ BOJ1931.py
@@ -0,0 +1,21 @@
+N = int(input())
+room = [[] for i in range(N)]
+count = 1
+fk = 0
+k = 1
+temp = 0
+for i in range(N):
+    a, b = map(int, input().split())
+    room[i].append(a)
+    room[i].append(b)
+    
+room.sort(key = lambda x : x[1]) # key = lambda x : x[] X기준으로 정렬
+
+while k < N:
+    if room[fk][1] <= room[k][0]:
+        count += 1
+        temp = fk
+        fk = k
+        k = temp
+    k += 1
+print(count)

41d58c8eb43d417c26f315bb52b42154ad4f684a
ralph <sanghwaj222@gmail.com>
2023-11-17 22:03:01 +0900

    231117 sort Silver1not yet

--- BOJ1931.py
+++ BOJ1931.py
@@ -3,17 +3,24 @@ room = [[] for i in range(N)]
 count = 1
 fk = 0
 k = 1
-temp = 0
 for i in range(N):
     a, b = map(int, input().split())
     room[i].append(a)
     room[i].append(b)
     
-room.sort(key = lambda x : x[1]) # key = lambda x : x[] X기준으로 정렬
-
+room.sort(key = lambda x : x[1]) # key = lambda x : x[] X기준으로 정렬, x[1]이 다 같은 경우엔 정렬이 제대로 안됨
+                                 # ex) (4,4), (3,4), (2,4)
+                                 # ex) (3,6), (2,6), (1,6)
 while k < N:
     if room[fk][1] <= room[k][0]:
         count += 1
         fk = k
+    elif room[fk][0] == room[fk][1]:
+        for i in range(fk+1, N-fk):
+            if room[fk][1] <= room[i][1]:
+                count += 1
+                fk = i
+                break
     k += 1
+    
 print(count)

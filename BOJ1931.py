743b8f8054187f923e92c63bb56ae04a2b29f4ea
ralph <sanghwaj222@gmail.com>
2023-11-17 21:42:27 +0900

    231117 sort Silver1

--- BOJ1931.py
+++ BOJ1931.py
@@ -14,8 +14,6 @@ room.sort(key = lambda x : x[1]) # key = lambda x : x[] X기준으로 정렬
 while k < N:
     if room[fk][1] <= room[k][0]:
         count += 1
-        temp = fk
         fk = k
-        k = temp
     k += 1
 print(count)

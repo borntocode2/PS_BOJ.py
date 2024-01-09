29196c142a471970a1f2b48cd56e70ccd5d4774a
sanghwa <sanghwaj222@gmail.com>
2024-01-10 00:20:15 +0900

    240110 체스판BF

--- Brute Force/1018체스판BF.py
+++ Brute Force/1018체스판BF.py
@@ -1,39 +1,35 @@
 N, M = map(int, input().split())
 Chesspan = []
-fixdot = 0
-mindot = 1000000
+answerdot = 10000
 fixed_N = N - 7
 fixed_M = M - 7
+# BBBWBWBW 00 02 04 06 // 01 03 05 07
+# BBBBWBWB 10 12 14 16 // 11 13 15 17
+# BBBWBWBW 20 22 24 26 // 21 23 25 27
+# BBBBWBWB 30 32 34 36 // 31 33 35 37
 
-# x = 0 y = 5
-# BBBWBWBW 1
-# BBBBWBWB 6
-# BBBWBWBW 1
-# BBBBWBWB 6
-# BBBWBWBW 1
-# BBBBWBWB 6
-# BBBWBWBW 1
-# BBBBWBWB 6
+
+
+
 def BF(x, y):
-    global fixdot
-    fixdot = 0
+    global coldot, rowdot, realdot
+    coldot = 0
+    rowdot = 0
+    realdot = 0
     for i in range(8):
         for j in range(8):
-            if Chesspan[i][y] == "B":
-                if j % 2 == 0:
-                    if Chesspan[i][y+j] != "B":
-                        fixdot += 1
+            if (i + j) % 2 == 0:
+                if Chesspan[x+i][y+j] != "W":
+                    coldot += 1
                 else:
-                    if Chesspan[i][y+j] != "W":
-                        fixdot += 1
+                    rowdot += 1
             else:
-                if j % 2 == 0:
-                    if Chesspan[i][y+j] != "W":
-                        fixdot += 1
+                if Chesspan[x+i][y+j] != "B":
+                    coldot += 1
                 else:
-                    if Chesspan[i][y+j] != "B":
-                        fixdot += 1
-
+                    rowdot += 1
+                    
+    realdot = min(coldot, rowdot)
 
                         
         
@@ -43,5 +39,8 @@ for i in range(N):
 for i in range(fixed_N):
     for j in range(fixed_M):
         BF(i,j)
-        mindot = min(mindot, fixdot)
-print(mindot)
+        answerdot = min(answerdot, realdot)
+        
+print(answerdot)
+
+
--- test.py
+++ test.py
@@ -1,1 +1,8 @@
+<<<<<<< Updated upstream
 print(1 % 2)
+=======
+N = 4
+list = [10, 100, 1000, 10000]
+for i in range(N):
+    print(list[i])
+>>>>>>> Stashed changes

70f04c18ef108ba25286bb2e5b489d00ebcba7f7
sanghwa <sanghwaj222@gmail.com>
2024-01-12 01:54:10 +0900

    240112 N과M(1)

--- Brute Force/15649NMBF.py
+++ Brute Force/15649NMBF.py
@@ -1,23 +1,24 @@
 # 1부터 N 까지 자연수 중에서 중복 없이 M개를 고른 수열 
 # nPm 순열 
 
-def dfs(n, lst):
-  
-    if n == M: 
-        ans.append(lst)
-        return
-    
-    for j in range(1, N+1):
-        if visits[j] == 0:          
-            visits[j] = 1
-            dfs(n+1, lst+[j])
-            visits[j]=0
-    
-N, M = map(int, input().split())
-ans = [] 
-visits = [0]*(N+1) 
+N, M = map(int,input().split())
+answer = []
+visits = [0 for _ in range(N+1)]
 
-dfs(0, [])
-for lst in ans:
-    print(*lst)
+def DFS(m, lst):
+    if m == M:
+        answer.append(lst)
+        return
+    for j in range(1, N+1):
+        if visits[j] == 0:
+            visits[j] = 1
+            DFS(m+1, lst+[j])
+            visits[j] = 0
+
+DFS(0, [])
+
+for i in answer:
+    print(*i)
+
+
 

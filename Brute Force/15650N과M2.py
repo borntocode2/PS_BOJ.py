8032a000936537c02ef739f27c1d7f589589a8c4
ralph <sanghwaj222@gmail.com>
2024-01-12 18:13:58 +0900

    230112 n과m(2)ing

--- Brute Force/15650N과M2.py
+++ Brute Force/15650N과M2.py
@@ -29,4 +29,12 @@ for j in range(1, N+1):
 
 print(*answer)
 
+# 4 2
+
+# 1 2  / 1을 뽑고 2를 선택 return
+# 1 3  / 1 후에 2를 선택하지않고 3 return
+# 1 4  / 1 후에 2, 3 선택하지 않고 return 
+# 2 3  / 1은 visits처리가 되어있어야 함.
+# 2 4  / 
+# 3 4  / 1, 2은 visits처리가 되어 있어야함.
 

0a1d5dac165df64cd4b04c6395cd1e20b3716a8e
ralph <sanghwaj222@gmail.com>
2024-01-25 12:14:40 +0900

    2401125 N과M11

---
+++ Brute Force/15665N과M11.py
@@ -0,0 +1,30 @@
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+ans = []
+temp_ans = set()
+lst_ans = []
+arr.sort()
+
+def dfs(m, lst):
+    if m == M-1:
+        ans.append(lst)
+        return
+    
+    for j in range(N):
+        dfs(m+1, lst + [arr[j]])
+
+for i in range(N):
+    dfs(0, [arr[i]])
+
+for i in ans:
+    if isinstance(i, list):
+        temp_ans.add(tuple(i))
+
+set_ans = sorted(temp_ans)
+
+for i in set_ans:
+    if isinstance(i, tuple):
+        lst_ans.append(list(i))
+
+for i in lst_ans:
+    print(*i)

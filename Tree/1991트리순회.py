58cad0de5a3f02ff77465f086b34e143dfc0df4a
ralph <sanghwaj222@gmail.com>
2024-02-05 19:19:04 +0900

    240205 트리순회 Silver 1

--- Tree/1991트리순회.py
+++ Tree/1991트리순회.py
@@ -9,3 +9,39 @@
 # 전위 순회한 결과 : ABDCEFG // (루트) (왼쪽 자식) (오른쪽 자식)
 # 중위 순회한 결과 : DBAECFG // (왼쪽 자식) (루트) (오른쪽 자식)
 # 후위 순회한 결과 : DBEGFCA // (왼쪽 자식) (오른쪽 자식) (루트)
+N = int(input())
+tree = {}
+answer = []
+for i in range(N):
+    root, left, rigth = map(str, input().split())
+    tree[root] = [left, rigth]
+
+def preorder(root):
+    if root != ".":
+        answer.append(root)
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+def inorder(root):
+    if root != ".":
+        inorder(tree[root][0])
+        answer.append(root)
+        inorder(tree[root][1])
+    
+def postorder(root):
+    if root != ".":
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        answer.append(root)
+
+preorder("A")
+print(*answer, sep = "")
+answer.clear()
+
+inorder("A")
+print(*answer, sep = "")
+answer.clear()
+
+postorder("A")
+print(*answer, sep = "")
+    

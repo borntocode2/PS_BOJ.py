6c3a03952ef7b665f75349813f430cdf9f2c1708
ralph <sanghwaj222@gmail.com>
2024-02-22 16:54:37 +0900

    240222 1571Greedy Gold 4

---
+++ Greedy/1715카드정렬하기.py
@@ -0,0 +1,20 @@
+import heapq
+heap = []
+sum = 0
+N = int(input())
+for i in range(N):
+     heapq.heappush(heap, int(input())) # heapq.heappush 는 변수에 따라 변하지 않는 함수
+     
+while len(heap) >= 2:
+    a = heapq.heappop(heap)
+    b = heapq.heappop(heap)
+    tempsum = a + b
+    heapq.heappush(heap, tempsum)
+    sum += tempsum
+
+
+print(sum)
+    
+
+
+# 1 2 3 4

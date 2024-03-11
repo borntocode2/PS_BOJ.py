89d21b233e91d3df8a62b5f76d30011143b8881c
ralph <sanghwaj222@gmail.com>
2024-03-11 20:33:51 +0900

    240311 11000heap Gold5

---
+++ priority queue/11000강의실배정.py
@@ -0,0 +1,23 @@
+import heapq as hq
+N = int(input())
+time = 0
+hip = []
+arr = []
+for i in range(N):
+    arr.append(list(map(int, input().split())))
+arr.sort()
+hq.heappush(hip, arr[0][1])
+
+for i in range(1, N):
+    if arr[i][0] >= hip[0]:
+        hq.heappush(hip, arr[i][1])
+        hq.heappop(hip)
+    else:
+        hq.heappush(hip, arr[i][1])
+
+print(len(hip))
+#반례
+#3
+#1 4
+#2 3
+#3 5

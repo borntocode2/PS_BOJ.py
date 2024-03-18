c8f243f2e1c0c3607e45245dc3e0ea4c6d2ac9f4
ralph <sanghwaj222@gmail.com>
2024-03-18 16:56:03 +0900

    240318 PQ Gold2

--- priority queue/1655가운데를말해요.py
+++ priority queue/1655가운데를말해요.py
@@ -1,22 +1,37 @@
 # 숫자 하나가 추가될 때 마다의 리스트의 중간 값을 추가한다.
 # 최댓값 힙 [a, b, c, d] 최솟값 힙 [e, f, g, h] 에서 d를 출력하도록 해야한다.
 import heapq as hq
+import sys
+input = sys.stdin.readline
+left_hip = []
+right_hip = [] # maxheap
 
-hip = []
 N = int(input())
 
 for i in range(N):
     a = int(input())
-    hq.heappush(hip, a)
-    
-    print(hip)
-    if len(hip)%2 == 0:
-        print("가운데 수는: ", min(hip[len(hip) // 2], hip[(len(hip) // 2) - 1]))
+    if len(left_hip) >= 1:
+        dot = left_hip[0]
+        if i % 2 == 0:
+            hq.heappush(left_hip, -a)
+            if -dot <= a:
+                right = hq.heappop(right_hip)
+                hq.heappush(right_hip,-hq.heappop(left_hip))
+                hq.heappush(left_hip, right)
 
+
+        else:
+            hq.heappush(right_hip, a)
     else:
-        print("가운데 수는: ", hip[len(hip) // 2])
-
-#7
+        hq.heappush(left_hip, -a)
+    print(-left_hip[0])
+#-10000 10000 100 -100 1 -1 -1 1 0 3 1
+        
+        
+            
+        
+  
+    
         
 #1
 #5
